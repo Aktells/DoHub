95dcfbcd4465009b284ab7e5829032ec43a7ced6
Apoorv Khandelwal <139608736+Aktells@users.noreply.github.com>
2025-08-30 21:50:02 +0530

    Update model.py

--- DoHub/pages/model.py
+++ DoHub/pages/model.py
@@ -2,6 +2,10 @@
 import streamlit as st
 import pandas as pd
 from bot import get_bot_response, filter_candidates
+import sys, importlib
+if "bot" in sys.modules:
+    importlib.reload(sys.modules["bot"])
+import bot
 if not st.session_state.get("auth", False):
     st.warning("Please log in first.")
     st.switch_page("home.py")   # send them back
@@ -113,3 +117,4 @@ st.divider()
 st.caption(
     "Tip: try to use the form"
 )
+
