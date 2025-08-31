7029b884834becfe22a37fc6038f6aa528c168d7
Apoorv Khandelwal <139608736+Aktells@users.noreply.github.com>
2025-08-31 10:55:29 +0530

    Update model.py

--- DoHub/pages/model.py
+++ DoHub/pages/model.py
@@ -4,7 +4,6 @@ import pandas as pd
 from bot import get_bot_response, filter_candidates
 import sys, importlib
 import os
-st.sidebar.text(f"API key present: {bool(os.getenv('OPENAI_API_KEY'))}")
 if not st.session_state.get("auth", False):
     st.warning("Please log in first.")
     st.switch_page("home.py")   # send them back
@@ -116,6 +115,7 @@ st.divider()
 st.caption(
     "Tip: try to use the form"
 )
+
 
 
 
